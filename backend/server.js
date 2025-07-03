542caedbbe8cd432a9b1bb73bcb51e00fee415dc
sagar giri <sagargiri456>
2025-07-03 19:03:03 +0530

    devpro

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // ✅ Middlewares
 app.use(cors({
-  origin: "https://devhelp-taupe.vercel.app", // ✅ replace with your actual frontend URL
+  origin: ["http://localhost:5173","https://devhelp-taupe.vercel.app"], // ✅ replace with your actual frontend URL
   credentials: true,
 }));
 
